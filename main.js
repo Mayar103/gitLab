986e50dc81d5932b24f3011cb3ff8f90ac4c498d
Mennat Allah <manoshkamal316@gmail.com>
2023-01-28 15:43:20 +0200

    mennaRequest

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-app.listen(3000)
+app.get("/menna", function (req, res) {
+  res.send("hi menna");
+});
+
+app.listen(3000);
